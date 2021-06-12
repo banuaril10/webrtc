43fd5ef4034c789c9e0737bbda83830486a2fd54
banuaril10 <42630911+banuaril10@users.noreply.github.com>
2021-06-12 17:54:57 +0700

    upload 2

---
+++ build/precache-manifest.a072002029e3c9c3727ec29d47ed5b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a31513a0704a958a243aa575e9d224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15cdd7d46bda7da6f4d7",
+    "url": "/static/css/main.b93526a0.chunk.css"
+  },
+  {
+    "revision": "e5d5038a1af446718fa0",
+    "url": "/static/js/2.bd8ffd8c.chunk.js"
+  },
+  {
+    "revision": "15cdd7d46bda7da6f4d7",
+    "url": "/static/js/main.ec393888.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  }
+]);
