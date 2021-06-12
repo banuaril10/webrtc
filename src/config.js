e3c549335ab891abc0b81159caf77a722743f8a3
banuaril10 <42630911+banuaril10@users.noreply.github.com>
2021-06-12 17:50:20 +0700

    upload

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: 'AIzaSyCx8Zg7rsEJkEn7pFAYtITwsG_Z73e8ABM',
+  authDomain: 'webrtc-dcb75.firebaseapp.com',
+  databaseURL: 'https://webrtc-dcb75-default-rtdb.firebaseio.com',
+  projectId: 'webrtc-dcb75',
+  storageBucket: 'webrtc-dcb75.appspot.com',
+  messagingSenderId: '302107195305',
+  appId: '1:302107195305:web:392f876384cf180d37b6b8'
+}
+
+export default config
